494b168600dd7fa81ff3464a72e1543bb4909110
muk <themu321@gmail.com>
2025-11-15 11:13:29 -0500

    Created react app

---
+++ sagafe/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.ebayimg.com/images/g/UKIAAOSwLOViWckO/s-l1200.jpg" className="App-logo" alt="logo" />
+        <p>
+          Saga Home
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
