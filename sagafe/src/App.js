ae32ae99ea83a6d276308fa9bdc598535806ce1e
muk <themu321@gmail.com>
2025-11-27 18:35:11 -0500

    stuff

--- sagafe/src/App.js
+++ sagafe/src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/")
+      .then(res => {
+        setMessage(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error("API error:", err);
+        setMessage("Error fetching API");
+      });
+      
+
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://i.ebayimg.com/images/g/UKIAAOSwLOViWckO/s-l1200.jpg" className="App-logo" alt="logo" />
-        <p>
-          Saga Home
+        <img 
+          src="https://i.ebayimg.com/images/g/UKIAAOSwLOViWckO/s-l1200.jpg" 
+          className="App-logo" 
+          alt="logo" 
+        />
+        <p>Saga Home</p>
+
+        {/* Display API result */}
+        <p style={{ marginTop: '20px', fontSize: '20px' }}>
+          {message || "Loading..."}
         </p>
       </header>
     </div>
